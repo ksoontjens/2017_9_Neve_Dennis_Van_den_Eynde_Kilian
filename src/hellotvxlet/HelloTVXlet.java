197328f1e99e03250a995076ad4765e254c2cdf7
ksoontjens <koen.soontjens@kdg.be>
2017-05-19 09:35:33 +0200

    eerste commit

---
+++ src/hellotvxlet/HelloTVXlet.java
@@ -0,0 +1,61 @@
+package hellotvxlet;
+
+import java.awt.event.ActionEvent;
+import java.util.Timer;
+import javax.tv.xlet.*;
+import org.dvb.event.EventManager;
+import org.dvb.event.UserEventRepository;
+import org.havi.ui.HContainer;
+import org.havi.ui.HScene;
+import org.havi.ui.HSceneFactory;
+import org.havi.ui.HStaticText;
+import org.havi.ui.event.HActionListener;
+
+
+public class HelloTVXlet implements Xlet, HActionListener {
+
+    HScene scene;
+     Playfield bord;
+    public HelloTVXlet() {
+        
+    }
+
+    public void initXlet(XletContext context) { //720 x 576
+      scene=HSceneFactory.getInstance().getDefaultHScene();
+     bord=new Playfield();
+         UserEventRepository repo=new UserEventRepository("repo");
+         repo.addAllArrowKeys();
+     EventManager man=EventManager.getInstance();
+     man.addUserEventListener(bord, repo);
+   
+      scene.add(bord);
+      scene.validate();
+      scene.setVisible(true);
+
+      scene.repaint();
+    }
+
+    public void callback()
+    {
+        bord.run();
+    }
+    public void startXlet() {
+           MijnTimerTask mtt=new MijnTimerTask();
+           mtt.setCB(this);
+           Timer t=new Timer();
+           t.scheduleAtFixedRate(mtt, 0, 500);
+    }
+
+    public void pauseXlet() {
+     
+    }
+
+    public void destroyXlet(boolean unconditional) {
+     
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+       
+        }
+        
+    }
---
+++ src/hellotvxlet/Playfield.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hellotvxlet;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import org.bluray.ui.event.HRcEvent;
+import org.dvb.event.UserEvent;
+import org.dvb.event.UserEventListener;
+import org.dvb.event.UserEventRepository;
+import org.havi.ui.HComponent;
+
+/**
+ *
+ * @author student
+ */
+public class Playfield extends HComponent implements UserEventListener {
+
+    ArrayList slang=new ArrayList();
+    double hx=350;
+    double hy=300;
+    double hoek=0;
+    double gr=50;
+    
+    public Playfield()
+    {
+        this.setBounds(0,0,720,576); // full screen
+        
+    }
+    
+   
+    public void paint(Graphics g)
+    {
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,720,576);
+     
+        g.setColor(Color.WHITE);
+                double radhoek=(hoek/360.0)*2*Math.PI;
+                double hoek2=hoek+120;
+                double radhoek2=(hoek2/360.0)*2*Math.PI;
+                double hoek3=hoek-120;
+                double radhoek3=(hoek3/360.0)*2*Math.PI;
+                
+                int x[]=new int[3];
+                int y[]=new int[3];
+                 x[0]=(int)(hx+gr*Math.cos(radhoek));
+                 x[1]=(int)(hx+gr*Math.cos(radhoek2));
+                 x[2]=(int)(hx+gr*Math.cos(radhoek3));
+                
+                 y[0]=(int)(hx+gr*Math.sin(radhoek));
+                 y[1]=(int)(hx+gr*Math.sin(radhoek2));
+                 y[2]=(int)(hx+gr*Math.sin(radhoek3));
+    
+                
+            g.drawPolygon(x, y, 3);
+            
+        
+    }
+
+    
+    public void run()
+    {
+
+        this.repaint();
+    }
+    public void userEventReceived(UserEvent e) {
+  if (e.getType()==HRcEvent.KEY_PRESSED)
+  {
+      if (e.getCode()==HRcEvent.VK_LEFT)
+      {
+          hoek--;
+          this.repaint();
+      }
+  }
+}
+}
+
